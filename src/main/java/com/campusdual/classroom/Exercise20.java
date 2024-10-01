01d34db9606c8595e596a227d1fb05a7a01baafb
Alberto Salvado Fernández <95777814+AlberSalva@users.noreply.github.com>
2024-10-01 11:00:43 +0200

    Implemented getPeopleList() and showPeopleDetails()

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -6,11 +6,23 @@ import java.util.List;
 public class Exercise20 {
 
     public static List<Person> getPeopleList() {
+        ArrayList<Person> PeopleList = new ArrayList<Person>();
+        Person p = new Person("John", "Smith");
+        Person t = new Teacher("María", "Montessori", "Educación");
+        Person po = new PoliceOfficer("Jake", "Peralta", "B-99");
+        Person d = new Doctor("Gregory", "House", "Nefrología e infectología");
+        PeopleList.add(p);
+        PeopleList.add(t);
+        PeopleList.add(po);
+        PeopleList.add(d);
 
+        return PeopleList;
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
-
+        for (int i = 0; i < stringList.size(); i++) {
+            stringList.get(i).getDetails();
+        }
     }
 
     public static void main(String[] args) {
